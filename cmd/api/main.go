4bf2df9b9c47766b8704eb59d94bc312703a1bc4
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-22 22:40:33 +0530

    Feat: initilized fsNotify watcher

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
 
 func main() {
-	fmt.Println("hello world")
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					fmt.Println("!ok")
+					break
+				}
+				log.Println("event", event)
+				if event.Has(fsnotify.Write) {
+					log.Println("modified file ", event)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					log.Println("ok not")
+					return
+				}
+				log.Println("erro", err)
+			}
+
+		}
+
+	}()
+
+	err = watcher.Add("testDir/test.txt")
+	if err != nil {
+		log.Fatal("err", err)
+	}
+	<-make(chan struct{})
 }
